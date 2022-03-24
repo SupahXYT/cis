21787b54d204504d92d98dda7a30cdf225193eb6
SupahXYT <josuefirelol@gmail.com>
2022-03-24 15:39:37 -0700

    asd

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ left_motor = Motor(Port.A)
 right_motor = Motor(Port.B)
 arm = Motor(Port.C)
 gyro = GyroSensor(Port.S1)
-touch_sensor = TouchSensor(Port.S4)
+# touch_sensor = TouchSensor(Port.S4)
+color_sensor = ColorSensor(Port.S4)
+touch_sensor = TouchSensor(Port.S3)
 
 # drive = DriveBase(left_motor, right_motor)
 
@@ -93,18 +95,8 @@ def correct():
 
 def roomba():
     arm.hold()
-    while True:
-        left_motor.run(300)
-        right_motor.run(300)
-        if touch_sensor.pressed():
-            correct()
+    ev3.speaker.say("Initiating cleaning of current space")
 
-from random import random 
-def randint(min, max):
-    return round((max-min)*random()+min)
-
-def proomba():
-    arm.hold()
     while True:
         left_motor.run(300)
         right_motor.run(300)
@@ -113,9 +105,120 @@ def proomba():
             right_motor.stop()
             left_motor.run_time(-300, 1000, wait=False)
             right_motor.run_time(-300, 1000, wait=False)
-            time.sleep(1.2)
-            direction = randint(0, 1)
-            mturn(90*direction + -90*(not direction))
+            time.sleep(1.1)
+            mturn(90)
+
+
+from random import random 
+def randint(min, max):
+    return round((max-min)*random()+min)
+
+def proomba():
+    arm.hold()
+    ev3.speaker.say("Must clean now clean room move clean")
+
+    while True:
+        left_motor.run(300)
+        right_motor.run(300)
+        if touch_sensor.pressed():
+            left_motor.stop()
+            right_motor.stop()
+            left_motor.run_time(-300, 1000, wait=False)
+            right_motor.run_time(-300, 1000, wait=False)
+            time.sleep(1.1)
+            mturn(randint(-90, 90))
+
+void = None
+
+def color_sensor_one():
+    while True: 
+        ev3.screen.print(color_sensor.ambient())
+        if color_sensor.ambient() < 3:
+            ev3.speaker.say("the world has gone dark, my time has come")
+        time.sleep(.1)
+
+def mode_switcher():
+    while True: 
+        ev3.screen.print("entering ambient\nmode")
+        ev3.speaker.say("entering ambient mode")
+        while True: 
+            ev3.screen.print(color_sensor.ambient())
+            if touch_sensor.pressed():
+                break
+        ev3.screen.print("entering reflective\nmode")
+        ev3.speaker.say("entering reflective mode")
+        while True:
+            ev3.screen.print(color_sensor.reflection())
+            if touch_sensor.pressed():
+                break
+
+def tape_sensor():
+    while True: 
+        color = color_sensor.color()
+        if color == Color.BLUE:
+            ev3.screen.print("Color is blue")
+        elif color == Color.YELLOW:
+            ev3.screen.print("Color is yellow")
+        else:
+            ev3.screen.print("Color not blue or yellow")
+
+def count_blue():
+    previous_color = color_sensor.color()
+    count = 0
+
+    while True: 
+        current_color = color_sensor.color()
+        over_blue = (current_color == Color.BLUE)
+
+        if(previous_color != current_color and over_blue):
+            count += 1
+        ev3.screen.print("Count: " + str(count))
+        previous_color = current_color
+
+def sustained_color(previous_colors, color, threshold):
+    return (previous_colors == [color for i in range(threshold)])
+
+class ColorMemory:
+    def __init__(self, size):
+        self.colors = []
+        self.size = size
+    
+    def add(self, color):
+        if len(self.colors) < self.size:
+            self.colors.insert(color, 0)
+        else:
+            self.colors.pop()
+            self.colors.insert(color, 0)
+
+    def get_last_color(self):
+        return self.colors[0]
+
+    def sustained(self, color):
+        for c in self.colors:
+            if c != color: 
+                return False 
+        return True
+
+def blue_yellow():
+    left_motor.run(100)
+    right_motor.run(100)
+    colors = ColorMemory(4)
+    colors.add(color_sensor.color())
+    count = 0
+
+    while True:
+        current_color = color_sensor.color()
+        if current_color == Color.YELLOW:
+            break
+        
+        if(colors.get_last_color() != current_color and colors.sustained(Color.BLUE)):
+            count += 1
+        ev3.screen.print("Count: " + str(count))
+        previous_colors.append(current_color)
+    
+    ev3.screen.print("Final count: " + str(count))
+    left_motor.hold()
+    right_motor.hold()
 
 def main():
     # turn_in_place(360)
@@ -128,10 +231,17 @@ def main():
     time.sleep(10)
     # move_distance(100)
 if __name__ == '__main__':
-    main()
+    # mode_switcher()
+    # tape_sensor()
+    # count_blue()
+    blue_yellow()
+
+    # main()
 
 # A.run(100)
 # B.run(100)
 # C.run_time(3000, 2000, then=Stop.HOLD, wait=False)
-# B.run_time(3000, 2000, then=Stop.HOLD)
+# B.run_time(identifier3000, 2000, then=Stop.HOLD)
+# discretely 
+# oh so you think you're ZQ
 
